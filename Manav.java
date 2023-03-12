c0ac27c44a76d69a355efb7c13ce9952046879e7
afifeyarenn <110766917+afifeyarenn@users.noreply.github.com>
2023-03-12 21:38:13 +0300

    Add files via upload

---
+++ Manav.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Manav {
+    public static void main(String[] args) {
+
+        double armut , elma , domates , muz, patlican;
+
+        Scanner inp = new Scanner(System.in);
+
+
+        System.out.print("Armut Kaç Kilo? : ");
+        armut = inp.nextDouble();
+
+        System.out.print("Elma Kaç Kilo? : ");
+        elma = inp.nextDouble();
+
+        System.out.print("Domates Kaç Kilo? : ");
+        domates = inp.nextDouble();
+
+        System.out.print("Muz Kaç Kilo? : ");
+        muz = inp.nextDouble();
+
+        System.out.print("Patlıcan Kaç Kilo? :");
+        patlican = inp.nextDouble();
+
+        double sonuc =  (armut*2.14)+(elma*3.67)+(domates*1.11)+(muz*0.95)+(patlican*5.00);
+        System.out.print("Toplam Tutar : " + sonuc);
+
+
+
+
+
+    }
+}
